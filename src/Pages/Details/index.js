f3deff154a34f4cab001cc5b98191756e169f791
mennatullah <mennaash5@gmail.com>
2023-09-17 12:50:25 +0300

    add all card

--- src/Pages/Details/index.js
+++ src/Pages/Details/index.js
@@ -8,6 +8,7 @@ import style from './style.css'
 function Details() {
     const { id } = useParams();
     const [course, setCourse] = useState({});
+    const [relatedCourse, setRelatedCourse] = useState({});
 
     useEffect(() => {
         async function fetchCourse() {
@@ -17,12 +18,32 @@ function Details() {
                     throw new Error("Network Error");
                 }
                 const courseData = await response.json();
+
+                console.log(courseData);
+
                 setCourse(courseData);
             } catch (error) {
                 console.error(error);
             }
         }
         fetchCourse();
+
+        async function fetchRelatedCourse() {
+            try {
+                const response = await fetch(`https://my-json-server.typicode.com/MennatullahAsh/goldenApi-Diploma/diplomaA`);
+                if (!response.ok) {
+                    throw new Error("Network Error");
+                }
+                const relatedCourseData = await response.json();
+
+                console.log(relatedCourseData);
+
+                setRelatedCourse(relatedCourseData);
+            } catch (error) {
+                console.error(error);
+            }
+        }
+        fetchRelatedCourse();
     }, [id]);
 
     return (
@@ -44,18 +65,19 @@ function Details() {
 
                 <div className='img'>
                     {course.url && <img src={course.url} alt={course.title} style={{ maxWidth: '80%' }} />}
-
-
                 </div>
-
             </div>
+
+            {/* عرض البيانات من الـAPI الثاني */}
+            <div className='related-course'>
+                <h2>دورة ذات صلة</h2>
+                <p>اسم الدورة: {relatedCourse.title}</p>
+                <p>مدة الدورة: {relatedCourse.duration}</p>
+            </div>
+
             <div className='SignD'>
-                <Link to={`/Landing`} size="small"
-                    color="primary" className='back'
-                > رجوع </Link>
-                <Link to={`/Sign`} size="small"
-                    color="primary" className='btnSignD'
-                >  سجل الآن  </Link>
+                <Link to={`/Landing`} size="small" color="primary" className='back'> رجوع </Link>
+                <Link to={`/Sign`} size="small" color="primary" className='btnSignD'>  سجل الآن  </Link>
             </div>
             <div style={{ height: '200px' }}></div>
             <Footer />
